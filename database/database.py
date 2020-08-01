34af5a6a6901a473f84d245ccd2acd2b4d0d4e68
King controller <67323151+akshay-rgb@users.noreply.github.com>
2020-08-01 20:04:37 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,1 +1,63 @@
- import os class Config(object): # get a token from https://chatbase.com CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "") # get a token from @BotFather TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "") # The Telegram API things APP_ID = int(os.environ.get("APP_ID", 12345)) API_HASH = os.environ.get("API_HASH") # Get these values from my.telegram.org # Array to store users who are authorized to use the bot AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split()) # Banned Unwanted Members.. BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split()) # the download location, where the HTTP Server runs DOWNLOAD_LOCATION = "./DOWNLOADS" # Telegram maximum file upload size MAX_FILE_SIZE = 50000000 TG_MAX_FILE_SIZE = 2097152000 FREE_USER_MAX_FILE_SIZE = 50000000 # chunk size that should be used with requests CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128)) # default thumbnail to be used in the videos DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90") # proxy for accessing youtube-dl in GeoRestricted Areas # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061 HTTP_PROXY = os.environ.get("HTTP_PROXY", "") # https://t.me/hevcbay/951 OUO_IO_API_KEY = "" # maximum message length in Telegram MAX_MESSAGE_LENGTH = 4096 # set timeout for subprocess PROCESS_MAX_TIMEOUT = 3600 # watermark file DEF_WATER_MARK_FILE = "" # Sql Database url By [king] DB_URI = os.environ.get("DATABASE_URL", "")
+ from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+import os
+
+import threading
+import asyncio
+
+from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, UniqueConstraint, func
+
+
+from sample_config import Config
+
+
+def start() -> scoped_session:
+    engine = create_engine(Config.DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
+
+INSERTION_LOCK = threading.RLock()
+
+class Thumbnail(BASE):
+    __tablename__ = "thumbnail"
+    id = Column(Integer, primary_key=True)
+    msg_id = Column(Integer)
+    
+    def __init__(self, id, msg_id):
+        self.id = id
+        self.msg_id = msg_id
+
+Thumbnail.__table__.create(checkfirst=True)
+
+async def df_thumb(id, msg_id):
+    with INSERTION_LOCK:
+        msg = SESSION.query(Thumbnail).get(id)
+        if not msg:
+            msg = Thumbnail(id, msg_id)
+            SESSION.add(msg)
+            SESSION.flush()
+        else:
+            SESSION.delete(msg)
+            file = Thumbnail(id, msg_id)
+            SESSION.add(file)
+        SESSION.commit()
+
+async def del_thumb(id):
+    with INSERTION_LOCK:
+        msg = SESSION.query(Thumbnail).get(id)
+        SESSION.delete(msg)
+        SESSION.commit()
+
+async def get_thumb(id):
+    try:
+        t = SESSION.query(Thumbnail).get(id)
+        return t
+    finally:
+        SESSION.close()
