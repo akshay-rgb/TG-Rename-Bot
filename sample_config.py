d1f149486612589a526cb5c43e57c3ad8bf1164d
King controller <67323151+akshay-rgb@users.noreply.github.com>
2020-08-01 19:59:33 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -34,5 +34,5 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3600
     # watermark file
     DEF_WATER_MARK_FILE = ""
-    # Sql Database url
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    # Sql Database url By [Aiden]
+    DB_URI = os.environ.get("DATABASE_URL
